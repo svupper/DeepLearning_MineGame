34ba63e0b3338b5a48fd874b4732a34f1a67d6c9
svupper <sofiane.merbah.sorbonne@gmail.com>
2021-05-01 00:19:39 +0200

    up

--- main.py
+++ main.py
@@ -3,19 +3,23 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class Maze:
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def flip_Maze(maze):
+
+
 def create_MazeBomb():
-    maze = [[0,0,0,0],
-            [0,0,0,0],
-            [0,0,0,0]]
-    for i in map(maze): print(maze +"\n")
+    maze = ((0,0,0,0),
+            (0,0,0,0),
+            (0,0,0,0))
+    for i in list(maze): print(i)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     create_MazeBomb()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
