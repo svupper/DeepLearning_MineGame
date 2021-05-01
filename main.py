6e4cdcae1997bb93768d3160d484074a86b3b631
svupper <sofiane.merbah.sorbonne@gmail.com>
2021-05-01 02:00:54 +0200

    Gameplay creation

--- main.py
+++ main.py
@@ -2,40 +2,53 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 import random
 
 class Node:
-
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.Bomb = random.randint(0, 1)
         self.Neighboors = []
 
-    def get_Nodes(self):
-        return self.
+    def get_Neigh(self):
+        return self.Neighboors
+
+    def add_Neigh(self, node):
+        self.Neighboors.append(node)
+
+    def add_Neigh_List(self, nodes):
+        self.Neighboors + nodes
 
 class Maze:
     def __init__(self):
         self.Map = []
 
     def create_MazeBomb(self,N):
-        self.Map = []
-        for i in range(N*N):
-            self.Map.append(random.randint(0, 1))
+        self.N=N
+        for x in range(N):
+            for y in range(N):
+                row = []
+                n = Node(x, y)
+                row.append(n)
+
+            self.Map.append(row)
 
     def flip_Maze(self):
         self.Map = [(0,0,0,0),
-                (0,0,0,0),
-                (0,0,0,0)]
+                    (0,0,0,0),
+                    (0,0,0,0)]
 
     def print_Map(self):
-        for i in self.Map: print(i)
+        for i in range(self.N): print(self.Map[0:self.N])
 
 
 # Press the green button in the gutter to run the script.
 
 
 if __name__ == '__main__':
-    create_MazeBomb()
+    m = Maze()
+    m.create_MazeBomb(5)
+    m.print_Map()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
