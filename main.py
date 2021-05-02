73ac7a25d13f1866f93342bed62e57b33de8158d
svupper <sofiane.merbah.sorbonne@gmail.com>
2021-05-03 01:42:24 +0200

    getter and setter

--- main.py
+++ main.py
@@ -13,10 +13,20 @@ class Node:
         self.Neighboors = []
 
     def __str__(self):
-        return self.Bomb;
+        i = 0
+        for node in self.Neighboors:
+            if node.Bomb == 1:
+                i += 1
+        return i
 
-    def get_Neigh(self):
-        return self.Neighboors
+    def __setitem__(self, b, x, y, n):
+        self.Bomb = b
+        self.x = x
+        self.y = y
+        self.Neighboors = n
+
+    def __getitem__(self):
+        return self.Bomb, self.x, self.y, self.Neighboors
 
     def add_Neigh(self, node):
         self.Neighboors.append(node)
@@ -25,23 +35,34 @@ class Node:
         self.Neighboors + nodes
 
 class Maze:
-    def __init__(self):
+    def __init__(self, N=5):
         self.Map = []
+        self.displayed = []
+        self.create_MazeBomb(N)
+        self.create_flip()
+
+    def __str__(self):
+        return self.displayed
+
+    def __getitem__(self, x, y):
+        return self.Map[x][y]
+
+    def __setitem__(self,x,y):
+        node = self.Map[x][y]
+        self.displayed[x][y] = node
 
     def create_MazeBomb(self,N):
-        self.N=N
+        self.N = N
         for x in range(N):
             row = []
             for y in range(N):
-                #
                 n = Node(x, y)
                 row.append(n)
             self.Map.append(row)
 
     def create_flip(self):
-        self.displayed = []
         for row in self.Map:
-            r=[]
+            r = []
             for col in row:
                 r.append(".")
             self.displayed.append(r)
@@ -57,16 +78,29 @@ class Maze:
                 r.append(col.Bomb)
             print(r)
 
-    #def revealCase(self,x,y):
+    def revealCase(self,x,y):
+        for row in self.Map:
+            r=[]
+            j=0
+            for col in row:
+
+                print(row)
+                print(j)
+                if ((row==x) & (col==y)):
+                    r.append(self.Map[x][y].Bomb)
+                    print("ezan")
+                else:
+                    r.append('.')
+            print(r)
 
 
 
 if __name__ == '__main__':
     m = Maze()
-    m.create_MazeBomb(5)
-    m.create_flip()
-    m.print_Map()
-
+    #m.create_MazeBomb(5)
+    #m.create_flip()
+    #m.print_Map()
+    #m.revealCase(1,1)
     #m.flip_Maze()
     #while(1):
         #wait user move
