a62ebf37f875d752e141b2dda5b1235548177c88
svupper <sofiane.merbah.sorbonne@gmail.com>
2021-05-02 23:50:52 +0200

    Gameplay creation

--- main.py
+++ main.py
@@ -33,24 +33,42 @@ class Maze:
                 #
                 n = Node(x, y)
                 row.append(n)
-
             self.Map.append(row)
 
-    def flip_Maze(self):
-        self.Map = [(0,0,0,0),
-                    (0,0,0,0),
-                    (0,0,0,0)]
+    def create_flip(self):
+        self.displayed = []
+        for row in self.Map:
+            r=[]
+            for col in row:
+                r.append(".")
+            self.displayed.append(r)
 
     def print_Map(self):
+        for row in self.displayed:
+            print(row)
+
+    def print_realMap(self):
         for row in self.Map:
             r=[]
             for col in row:
                 r.append(col.Bomb)
             print(r)
 
+    #def revealCase(self,x,y):
+
 
 if __name__ == '__main__':
     m = Maze()
     m.create_MazeBomb(5)
+    m.create_flip()
     m.print_Map()
-
+    #m.flip_Maze()
+    #while(1):
+        #wait user move
+        #m.print_Map()
+        #reveal case
+        #check if bomb
+        #ifBomb
+            #endgame
+        #else
+            #continue
